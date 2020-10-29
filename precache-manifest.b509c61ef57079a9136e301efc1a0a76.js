89a62229b3409a59eeb23b799b92400bcc4e3f7d
Ravi Varshney <ravivarshney49@gmail.com>
2020-10-29 10:17:03 +0530

    gh-pages

---
+++ precache-manifest.b509c61ef57079a9136e301efc1a0a76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec79e9c57dcc005a9ec7b10219453f8",
+    "url": "/socialX/X.ico"
+  },
+  {
+    "revision": "71115253f06d675086d3",
+    "url": "/socialX/css/app.99523295.css"
+  },
+  {
+    "revision": "d8dbc97958a97ff4aa34",
+    "url": "/socialX/css/chunk-vendors.091c2430.css"
+  },
+  {
+    "revision": "b3b1a420c842f5a298ce07cf60a9dc27",
+    "url": "/socialX/img/XWritten.b3b1a420.png"
+  },
+  {
+    "revision": "63a4aecbd84e830529bde8c674c86ac0",
+    "url": "/socialX/index.html"
+  },
+  {
+    "revision": "71115253f06d675086d3",
+    "url": "/socialX/js/app.534eb344.js"
+  },
+  {
+    "revision": "d8dbc97958a97ff4aa34",
+    "url": "/socialX/js/chunk-vendors.a6f116ec.js"
+  },
+  {
+    "revision": "4252387a7ee73d3706734c7be0fb7a0b",
+    "url": "/socialX/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/socialX/robots.txt"
+  }
+]);
