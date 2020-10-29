961f0eaa609e26c88fd26abc565f7dde1380340b
Ravi Varshney <ravivarshney49@gmail.com>
2020-10-29 10:08:23 +0530

    new endpoint

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transpileDependencies: ['vuetify'],
   devServer: {
-    proxy: 'http://localhost:8000/'
+    proxy: 'http://ec2-3-21-103-10.us-east-2.compute.amazonaws.com:8000/'
   }
 }
